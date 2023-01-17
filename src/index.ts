3483b98c1681a8b601560255d033373fdc9ac33a
Artem Didenko <artemdidenko@advoncommerce.com>
2023-01-17 17:18:15 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { analitics } from './modules/analitics.js';
+
+const temp: string = 'Hello Node';
+
+console.log('Ura', temp);
+
+analitics('Your-u');
---
+++ src/modules/analitics.ts
@@ -0,0 +1,3 @@
+export const analitics = (par: string): void => {
+  console.log(par + '!!!');
+};
