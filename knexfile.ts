03f8ea95a5b7f62c7a192b4ed265331db3a5a06e
Artem Didenko <artemdidenko@advoncommerce.com>
2023-01-18 16:23:36 +0200

    fixed

--- knexfile.ts
+++ knexfile.ts
@@ -1,11 +1,11 @@
-import type { Knex } from 'knex';
+// const Knex = require('knex');
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
 
 const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
-const config: { [key: string]: Knex.Config } = {
+const config: { [key: string]: any } = {
   development: {
     client: 'postgresql',
     connection: {
