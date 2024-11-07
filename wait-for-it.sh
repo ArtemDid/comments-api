1f73c36503120f0dbd5f5863cf3d3336f5945062
ArtemDid <temadidenko27@gmail.com>
2024-11-07 11:36:07 +0200

    fixed-14 CI

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Use this script to test if a given TCP host/port are available
 
 WAITFORIT_cmdname=${0##*/}
 
@@ -51,7 +50,6 @@ wait_for()
 
 wait_for_wrapper()
 {
-    # In order to support SIGINT during timeout: http://unix.stackexchange.com/a/57692
     if [[ $WAITFORIT_QUIET -eq 1 ]]; then
         timeout $WAITFORIT_BUSYTIMEFLAG $WAITFORIT_TIMEOUT $0 --quiet --child --host=$WAITFORIT_HOST --port=$WAITFORIT_PORT --timeout=$WAITFORIT_TIMEOUT &
     else
